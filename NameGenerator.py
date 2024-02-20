644b4f46ba56d27005c5c4951fe468b322a9f222
K. M. Azwad Hossain <azwadkm22@gmail.com>
2024-02-21 00:22:02 +0600

    Added a few modifications to the name generator

--- NameGenerator.py
+++ NameGenerator.py
@@ -29,25 +29,50 @@ first = ['Asif', 'Sakib', 'Fahim', 'Sajib',
      "Jamal", "Tushar", "Fazle", "Tonmoy", "Haidar", "Moinul", "Shadab",
      "Irtiza", "Shoummo", "Nibir", "Shafin", "Safin", "Ninad", "Raiyan",
      "Alvi", "Himel", "Shahriar", "Ramim", "Sadman", "Mahee", "Mugdho",
-     "Omio", "Tawsif", "Sadik", "Jahirul", "Tasnim", "Ashraf", "Saif",
+     "Omio", "Tawsif", "Sadik", "Jahirul", "Tasnim", "Ashraf", "Saif", "Motaher",
      "Radi", "Sadi", "Ataur", "Atik", "Apurbo", "Arnab", "Ankon", "Ongkon",
      "Mahid", "Tauhid", "Zakir", "Iftikhar", "Noor", "Naseem", "Rishad", "Imrul",
-     "Mushfik", "Amir", "Kamrul", "Pranto", "Mosaddek"
+     "Mushfik", "Amir", "Kamrul", "Pranto", "Mosaddek", "Sarwar", "Mustahid", "Fazal"
      ]
 
-last = ['Chowdhury', 'Ahmed', 'Khan', 'Hossain', "Choudhuri",
+islamicLastNames = ['Chowdhury', 'Ahmed', 'Khan', 'Hossain', "Choudhuri",
  'Uddin', 'Hasan', 'Haque', "Haq", "Hoq", 'Mahmud', 'Islam', "Mohammad", "Mohammed", 
-  'Ali', 'Alam', 'Rahman', 'Ahmad', 'Khondokar', "Ur-Rahman", "Sheikh",
-  'Ahsan', 'Zaman', 'Sarker', 'Habib', 'Majumder', "Al-Ahsan", "Uzzaman", "Al-Amin",
-  "Sen", "Datta", "Sarkar", "Biswas", "Ghosh", "Roy", "Pal", "Kumar",
-  "Mia", "Siddique", "Karim", "Malik", "Iqbal", "Barman", "Karmakar", "Abdullah"
+  'Ali', 'Alam', 'Rahman', 'Ahmad', 'Khondokar', "Ur-Rahman", "Sheikh", "Amin",
+  'Ahsan', 'Zaman', 'Habib', "Al-Ahsan", "Uzzaman", "Al-Amin", "Bhuiyan",
+  "Mia", "Siddique", "Karim", "Malik", "Iqbal", "Abdullah", "Skider",
   ]
+
+# islamicLastNames = []
+hinduLastNames = [ "Barman", 'Sarker', "Sarkar", "Biswas", "Ghosh", "Roy", "Pal", "Kumar", "Sen", "Datta", "Karmakar", 'Chowdhury', "Choudhuri", 'Majumder', "Paul"]
 
 switchPossible = ["Mahmud", "Ahsan", "Hasan", "Abdullah", "Mohammad", "Mohammed", "Sheikh", "Kumar"]
 
 def getPlayerName():
+
+      three_part_name_prob = getProbabilisticAnswer(.15)
+
+      if three_part_name_prob == 1:
+            newFirstName = getProbabilisticAnswer(.2)
+            if newFirstName == 1:
+                  NewFirst = ["Kazi", "Syed", "Md.", "Mohammed", "K.M."]
+                  firstName = random.choice(NewFirst)
+                  middleName = random.choice(first)
+                  lastName = random.choice(islamicLastNames)
+                  if firstName in ["Md.", "Mohammed"]:
+                        while lastName in  ["Mohammad", "Mohammed"]:
+                              lastName = random.choice(islamicLastNames)
+                  return firstName + " " + middleName + " " + lastName
+            else:
+                  last = random.choice([islamicLastNames, hinduLastNames])
+                  firstName = random.choice(first)
+                  middleName = random.choice(last)
+                  lastName = random.choice(first)
+                  return  firstName + " " + middleName + " " + lastName
+      
       firstName = random.choice(first)
+      last = random.choice([islamicLastNames, hinduLastNames])
       lastName = random.choice(last)
+
       switch = 0
       if lastName in switchPossible:
             switch = getProbabilisticAnswer(.25)
