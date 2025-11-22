2a0779cd0cc604fe12a00d999e03c9743f76b2cc
K. M. Azwad Hossain <azwadkm22@gmail.com>
2025-11-22 19:59:42 +0600

    Modified code to match python convention

--- player_list_utils.py
+++ player_list_utils.py
@@ -1,118 +1,117 @@
-
-def get_player_subset(playerList, role):
+def get_player_subset(list_of_players, role: str):
     subList = []
     if role == "Opener":
-        for player in playerList:
+        for player in list_of_players:
             if player.batting_order == "Opener":
                 subList.append(player)
         return subList
     
     elif role == "Wicketkeeper":
-        for player in playerList:
+        for player in list_of_players:
             if player.position == "Wicketkeeper":
                 subList.append(player)
         return subList
     
     elif role == "Allrounder":
-        for player in playerList:
+        for player in list_of_players:
             if player.position == "Allrounder":
                 subList.append(player)
         return subList
     
     elif role == "Spinner":
-        for player in playerList:
+        for player in list_of_players:
             if (player.position == "Bowler" or player.position == "Allrounder") and player.bowling_type == "Spinner":
                 subList.append(player)
         return subList
 
     elif role == "Pacer":
-        for player in playerList:
+        for player in list_of_players:
             if (player.position == "Bowler" or player.position == "Allrounder") and player.bowling_type == "Pacer":
                 subList.append(player)
         return subList
     return subList
         
 
-def find_top_N_player_in_category(playerList, n, category):
+def find_top_N_player_in_category(list_of_players, n, category):
     # One case left to handle what if n is bigger than sublist
     if(category == "Fame"):
-        playerList = sorted(playerList, key=lambda Player: Player.fame, reverse=True)
-        TopNPlayers = playerList[:n]
-        return TopNPlayers
+        list_of_players = sorted(list_of_players, key=lambda Player: Player.fame, reverse=True)
+        top_N_players = list_of_players[:n]
+        return top_N_players
 
     elif(category == "Batting"):
-        playerList = sorted(playerList, key=lambda Player: Player.batting, reverse=True)
-        TopNPlayers = playerList[:n]
-        return TopNPlayers
+        list_of_players = sorted(list_of_players, key=lambda Player: Player.batting, reverse=True)
+        top_N_players = list_of_players[:n]
+        return top_N_players
     
     elif(category == "Bowling"):
-        playerList = sorted(playerList, key=lambda Player: Player.bowling, reverse=True)
-        TopNPlayers = playerList[:n]
-        return TopNPlayers
+        list_of_players = sorted(list_of_players, key=lambda Player: Player.bowling, reverse=True)
+        top_N_players = list_of_players[:n]
+        return top_N_players
     
     elif(category == "Price"):
-        playerList = sorted(playerList, key=lambda Player: Player.estimated_price, reverse=True)
-        TopNPlayers = playerList[:n]
-        return TopNPlayers
+        list_of_players = sorted(list_of_players, key=lambda Player: Player.estimated_price, reverse=True)
+        top_N_players = list_of_players[:n]
+        return top_N_players
     
     elif(category == "Pacer"):
-        subList = get_player_subset(playerList, "Pacer")
+        subList = get_player_subset(list_of_players, "Pacer")
         subList = sorted(subList, key=lambda Player: Player.bowling, reverse=True)
-        TopNPlayers = subList[:n]
-        return TopNPlayers
+        top_N_players = subList[:n]
+        return top_N_players
     
     elif(category == "Spinner"):
-        subList = get_player_subset(playerList, "Spinner")
+        subList = get_player_subset(list_of_players, "Spinner")
         subList = sorted(subList, key=lambda Player: Player.bowling, reverse=True)
-        TopNPlayers = subList[:n]
-        return TopNPlayers
+        top_N_players = subList[:n]
+        return top_N_players
     
     elif(category == "Wicketkeeper"):
-        subList =  get_player_subset(playerList, "Wicketkeeper")
+        subList =  get_player_subset(list_of_players, "Wicketkeeper")
         subList = sorted(subList, key=lambda Player: Player.batting, reverse=True)
-        TopNPlayers = subList[:n]
-        return TopNPlayers
+        top_N_players = subList[:n]
+        return top_N_players
 
     elif(category == "Allrounder"):
-        subList =  get_player_subset(playerList, "Allrounder")
+        subList =  get_player_subset(list_of_players, "Allrounder")
         subList = sorted(subList, key=lambda Player: (Player.batting + Player.bowling )/2, reverse=True)
-        TopNPlayers = subList[:n]
-        return TopNPlayers
+        top_N_players = subList[:n]
+        return top_N_players
     
     elif(category == "Opener"):
-        subList =  get_player_subset(playerList, "Opener")
+        subList =  get_player_subset(list_of_players, "Opener")
         subList = sorted(subList, key=lambda Player: Player.batting, reverse=True)
-        TopNPlayers = subList[:n]
-        return TopNPlayers
+        top_N_players = subList[:n]
+        return top_N_players
 
-def find_players_with_stats_above(playerList, stat, value):
-    lop = []
-    for player in playerList:
+def find_players_with_stats_above(list_of_players, stat, value):
+    lop_above = []
+    for player in list_of_players:
         if(stat == "Batting"):
             if player.batting > value:
-                lop.append(player)
+                lop_above.append(player)
 
         elif(stat == "Bowling"):
             if player.bowling > value:
-                lop.append(player)
+                lop_above.append(player)
 
         elif(stat == "Allrounder"):
             if (player.batting + player.bowling)/2 > value:
-                lop.append(player)
+                lop_above.append(player)
 
         elif(stat == "Spin"):
             if player.bowling > value and player.bowling_type == "Spinner":
-                lop.append(player)
+                lop_above.append(player)
 
         elif(stat == "Pace"):
             if player.bowling > value and player.bowling_type == "Pacer":
-                lop.append(player)
-    return lop
+                lop_above.append(player)
+    return lop_above
 
 
-def find_players_above_rating(playerList, rating):
+def find_players_above_rating(list_of_players, rating):
     players_above_rating = set()
-    for player in playerList:
+    for player in list_of_players:
         if player.batting > rating or player.bowling > rating:
             players_above_rating.add(player)
 
